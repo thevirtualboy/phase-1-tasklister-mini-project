ced9bf3b715cec3f0444296500db8fca8b9a18c2
Peter Fontana <dude0508@gmail.com>
2021-09-27 11:28:36 -0400

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form = document.querySelector('#create-task-form');
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      buildTask(e.target['new-task-description'].value);
+    });
 });
+
+function buildTask (task) {
+  let li = document.createElement('li')
+  li.textContent = `${task} `
+  document.querySelector('#tasks').appendChild(li)
+}
